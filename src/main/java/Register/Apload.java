51d9370556760ef45d5d5f966460eb3ef14ded37
Burull <sheishenalieva.burul@gmail.com>
2025-07-07 09:45:22 +0600

    jsxjis

---
+++ src/main/java/Register/Apload.java
@@ -0,0 +1,7 @@
+package Register;
+
+public class Apload {
+    public static void main(String[] args) {
+        System.out.println("apload");
+    }
+}
---
+++ src/main/java/Register/Register.java
@@ -0,0 +1,7 @@
+package Register;
+
+public class Register {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
