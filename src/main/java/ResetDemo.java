a0e93c4a4bf82cf2b9c93d9a9bb1818220c9f230
Burull <sheishenalieva.burul@gmail.com>
2025-07-09 02:45:30 +0600

    Version 1.0.4

--- src/main/java/ResetDemo.java
+++ src/main/java/ResetDemo.java
@@ -2,5 +2,8 @@ public class ResetDemo {
     public static void main(String[] args) {
         System.out.println("Version 1.0.0");
         System.out.println("Version 1.0.1");
+        System.out.println("Version 1.0.2");
+        System.out.println("Version 1.0.3--> Hello world");
+        System.out.println("Version 1.0.4");
     }
 }
